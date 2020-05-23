9bf1f69c2443cf22d085725df4c14b9c9257cb40
Düzgün <duzguntutar@gmail.com>
2020-05-24 00:07:08 +0300

    card widget

--- lib/ui/screens/home_view.dart
+++ lib/ui/screens/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ttaranimal/ui/widgets/animal_card.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -10,80 +11,75 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          expandedHeight: 200,
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-              title: Text(
-                "ANIMAL INTRODUCTION",
-                style: TextStyle(
-                    fontFamily: "IndieFlower",
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              centerTitle: false,
-              background: Image.network(
-                "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/101654047/original/faf99cd3c06f13ec1fdfe57cebb3fa89a5852519/draw-cartoons-for-you.png",
-                fit: BoxFit.scaleDown,
-              )),
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate((index, context) {
-          return Center(
-            child: Wrap(
-              spacing: 3.0,
-              children: <Widget>[
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-                getExpandedContainer(),
-              ],
-            ),
-          );
-        }, childCount: 1))
-      ],
+      slivers: _buildSliver(),
     ));
   }
 
-  Expanded getExpandedContainer() {
-    return Expanded(
-        flex: 1,
-        child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(5),
-            height: 190,
-            width: 190,
-            child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0)),
-                elevation: 10.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      height: 90,
-                      child: Image.network(
-                          "https://lh3.googleusercontent.com/proxy/t5uuOVzwrEoFHr_DhDJyQX59ftshzMLXxVsW3z0s_BbWHw8-iy-tsvt4zdPnJjqZEqV_HYRx-0crt-Q9XYRDSXki_leFAq0"),
-                    ),
-                    SizedBox(
-                      height: 1,
-                      child: Container(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Container(
-                        child: Text("LEON",
-                            style: TextStyle(
-                                fontFamily: "IndieFlower",
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold)))
-                  ],
-                ))));
+  List<Widget> _buildSliver() {
+    return <Widget>[
+      SliverAppBar(
+        expandedHeight: 200,
+        floating: true,
+        flexibleSpace: FlexibleSpaceBar(
+            title: Text(
+              "ANIMAL INTRODUCTION",
+              style: TextStyle(
+                  fontFamily: "IndieFlower",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            centerTitle: false,
+            background: Image.network(
+              "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/101654047/original/faf99cd3c06f13ec1fdfe57cebb3fa89a5852519/draw-cartoons-for-you.png",
+              fit: BoxFit.scaleDown,
+            )),
+      ),
+      SliverList(
+          delegate: SliverChildBuilderDelegate((index, context) {
+        return Center(
+          child: Wrap(
+            spacing: 3.0,
+            children: <Widget>[
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/pc78oEgRi.jpg",
+                name: "Kaplan",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/8izn5e8rT.jpg",
+                name: "Eşek",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/qcBo475Ri.jpg",
+                name: "Yengeç",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/8iEKeMyia.png",
+                name: "Arı",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/Biazn7Ai8.png",
+                name: "Köpek",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/ATbrEEkT4.gif",
+                name: "Eşek",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/kTMxba8Tj.png",
+                name: "Fok",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/di9yezbi7.jpg",
+                name: "Kaplan",
+              ),
+              AnimalCard(
+                animalImage: "http://clipart-library.com/images/zTXerqRTB.png",
+                name: "Maymun",
+              ),
+            ],
+          ),
+        );
+      }, childCount: 1))
+    ];
   }
 }
---
+++ lib/ui/widgets/animal_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AnimalCard extends StatelessWidget {
+  final String animalImage;
+  final String name;
+
+  AnimalCard({this.animalImage, this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(5),
+            height: 190,
+            width: 190,
+            child: buildAnimalCard()));
+  }
+
+  Card buildAnimalCard() {
+    return Card(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        elevation: 10.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            buildCarTopContainer(),
+            buildCardCenterSizedBox(),
+            buildCardBottomContainer()
+          ],
+        ));
+  }
+
+  SizedBox buildCardCenterSizedBox() {
+    return SizedBox(
+      height: 1,
+      child: Container(
+        color: Colors.grey,
+      ),
+    );
+  }
+
+  Container buildCardBottomContainer() {
+    return Container(
+        child: Text(name,
+            style: TextStyle(
+                fontFamily: "IndieFlower",
+                fontSize: 20,
+                fontWeight: FontWeight.bold)));
+  }
+
+  Container buildCarTopContainer() {
+    return Container(
+      height: 90,
+      child: Image.network(animalImage),
+    );
+  }
+}
